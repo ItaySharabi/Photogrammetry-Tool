b690278f9a22b4e139432d3d1c5443abf72b4edc
Itay Sharabi <itay.nextweb@gmail.com>
2022-06-13 20:29:21 +0300

    Tool implementation

--- scripts/DensePointCloud.py
+++ scripts/DensePointCloud.py
@@ -1,31 +1,47 @@
 #!/bin/bash
 import os
+import sys
+from tools import pretty_print
 
+DISCARD_OUTPUT = " > output.txt"
+MODEL_NAME = "Cowboy"
+DATASET_DIR = f"../TestDataset_{MODEL_NAME}/.*.jpg"
+
+if len(sys.argv) == 3:
+    DATASET_DIR = sys.argv[1]
+    MODEL_NAME = sys.argv[2]
 # This script is provided by a MicMac tutorial
 # which could be found here: https://micmac.ensg.eu/index.php/Fontaine_tutorial
 
-'''
-=============================================
-============= Tie-points search =============
-=============================================
 
+pretty_print('============================================='
+             '============= Tie-points search ============='
+             '=============================================')
+'''
 The tie points search for all the images should be computed
 simultaneously so all parts of images are linked with each other.
 The orientation will be in an arbitrary system, but the same system will be kept for all the images.
 The 4 depth maps generated (as well as the 4 clouds) will therefore be in the same coordinate system.
 The MulScale pattern allows to make a search for similar points firstly on sub-sampled images.
-On the TestDataset, it will be on 500pixels on the biggest side instead of 5472 on the originals images.
+On the TestDataset, it will be on 500pixels on the biggest side instead of 5472 on the original images.
 This allows to know which images have some tie points between them and to only
 run the tie-points search at a bigger resolution (here 2500) on the optimal sets of images.
 '''
 
-os.system('mm3d Tapioca MulScale "../TestDataset/IMG.*.JPG" 500 2500')
+pretty_print('This may take a while...')
+# Execute Tie-Point search using `Tapioca - a tool by `MicMac`
+# MulScale - Scale the images instead of their original size to be 600x2000
+os.system('mm3d Tapioca MulScale "../TestDataset/IMG.*.JPG" 600 2000')
 
+pretty_print('Tie-points search is finished!')
+pretty_print('\n')
+pretty_print('\n')
+pretty_print('\n')
 
+pretty_print('==================================================='
+             '=== Internal Orientation + Relative Orientation ==='
+             '===================================================')
 '''
-===================================================
-=== Internal Orientation + Relative Orientation ===
-===================================================
 We are now looking to know the position of the camera, relative to other images,
 For that in Tapas, there is the calibration type that we need to choose.
 Here, RadialStd is the pattern generally used for classic cameras.
@@ -39,8 +55,9 @@ At the last step, we can see that the image residual are for all the images lowe
 We also have to see the number of tie-points, as well as the percentage of keeping points ("99.8258 of 38466" : 99.8% of tie-points kept than 38466 calculated points).
 
 '''
-os.system('mm3d Tapas RadialStd "../TestDataset/IMG.*.JPG" Out=DroneTopic')
 
+pretty_print('This may take a while...')
+os.system('mm3d Tapas RadialStd "../TestDataset/IMG.*.JPG" Out=DroneTopic')
 
 '''
 =============================================
--- scripts/SparsePointCloud.py
+++ scripts/SparsePointCloud.py
@@ -1,7 +1,9 @@
 #!/bin/bash
 import os
 import sys
+from tools import pretty_print
 
+DISCARD_OUTPUT = " > output.txt"
 MODEL_NAME = "Cowboy"
 DATASET_DIR = f"../TestDataset_{MODEL_NAME}/.*.jpg"
 
@@ -11,16 +13,17 @@ if len(sys.argv) == 3:
 
 # mm3d Tapioca All ".*.JPG" 8
 # os.system(f'"TestDataset/IMG.*.JPG"')
-os.system(f'mm3d Tapioca All {DATASET_DIR} 1500')
+pretty_print('Generating Tie-Points... ')
+os.system(f'mm3d Tapioca All {DATASET_DIR} 1500 {DISCARD_OUTPUT}')
 
 
 # In MicMac, the tool which performs internal and relative orientation is called `Tapas`
-os.system(f'mm3d Tapas FraserBasic {DATASET_DIR} Out={MODEL_NAME}')  # 8 = #CPU's to use
+os.system(f'mm3d Tapas FraserBasic {DATASET_DIR} Out={MODEL_NAME} {DISCARD_OUTPUT}')  # 8 = #CPU's to use
 
 # Visualize relative orientation:
 # MicMac includes a tool which creates a sparse point cloud (TP)
 # for visualization. This tool is AperiCloud
-os.system(f'mm3d AperiCloud {DATASET_DIR} {MODEL_NAME}')  # 8 = #CPU's to use
+os.system(f'mm3d AperiCloud {DATASET_DIR} {MODEL_NAME} {DISCARD_OUTPUT}')  # 8 = #CPU's to use
 
 # 8 = #CPU's to use
 # os.system(f'mm3d GCPBascule {DATASET_DIR} {MODEL_NAME} Ground_Init Dico-Appuis.xml Mesure-Appuis.xml')
