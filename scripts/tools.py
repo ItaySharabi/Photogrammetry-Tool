90766b84183a829f8e5fd7cc0f7f09a377bf6661
Itay Sharabi <itay.nextweb@gmail.com>
2022-06-13 20:26:20 +0300

    extra methods for user interation (Keyboard input)

---
+++ scripts/tools.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+
+def pretty_print(content: str) -> None:
+    """ Prints the content with a nice delay
+        where the char `=` is printed out faster so
+        headlines consisting `=` look nice """
+    for c in content:
+        if "=" in c:
+            sleep(0.007)
+        else:
+            sleep(0.035)
+        print(c, end="", flush=True)
+    print("")
