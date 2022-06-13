4b173ec0f3e991ebf5e67aefe86e334e6c4d1ab7
Itay Sharabi <itay.nextweb@gmail.com>
2022-06-13 22:59:36 +0300

    Main tool script

--- SimpleMicMac.py
+++ SimpleMicMac.py
@@ -3,35 +3,44 @@ import sys
 from scripts.tools import *
 
 DISCARD_OUTPUT = " > output.txt"
-MODEL_NAME = "My3DModel"  # "Cowboy"
-DATASET_DIR = f"TestDataset/.*.jpg"
+MODEL_NAME = None  # "My3DModel"  # "Cowboy"
+DATASET_DIR = None  # f"TestDataset/.*.jpg"
 
 
 def menu() -> int:
     """ Show main tool menu and return user choice (int [1,4]) """
-    pretty_print('1. Generate Dense Point-Cloud')
-    pretty_print('2. Generate Depth Map')
-    pretty_print('3. Generate Densified Mesh Object')
-    pretty_print('4. Generate Orthomosiac')
+    print('1. Generate Dense Point-Cloud')
+    print('2. Generate Depth Map')
+    print('3. Generate Densified Mesh Object')
+    print('4. Generate Orthomosiac')
     return int(input())
 
 
 if __name__ == '__main__':
     os.system('cls')
-    pretty_print('  ~ SimpleMicMac Photogrammetry tool ~  ')
+    pretty_print('\n  ~ SimpleMicMac Photogrammetry tool ~  ')
     pretty_print(' ====================================== \n')
 
     pretty_print('All operations available in this tool are provided'
-                 ' by:\n\n `MicMac` - Open source 3D Photogrammetry software tools\n')
+                 ' by:\n\n ~ `MicMac` - Open source 3D Photogrammetry software tools ~ \n')
+
+    if len(sys.argv) != 3:
+        if DATASET_DIR is None:
+            pretty_print('Enter dataset directory path: ')
+            DATASET_DIR = input('Path: ')
+        if MODEL_NAME is None:
+            pretty_print('Enter model name: ')
+            MODEL_NAME = input('Model name: ')
 
     while True:
-
-        pretty_print('Which Operation would you like to choose?')
+        pretty_print('\nWhich Operation would you like to choose?')
         x = menu()
 
         if x == 1:
-            os.system('python scripts/DensifiedPointCloud.py')
-            pretty_print('Densified Point-Cloud generation finished')
+            os.system(f'python scripts/DensePointCloud.py {DATASET_DIR} {MODEL_NAME}')
+            pretty_print('\nDensified Point-Cloud generation finished...')
+            pretty_print('Goodbye!')
+            exit(0)
         elif x == 2:
             pass
 
